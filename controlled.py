7c24f3c3e6629d4d102d1551ec4846f719cb62e3
Simon Rhodes <simon.m.rhodes@gmail.com>
2025-04-21 20:08:26 -0400

    fixed bug

--- controlled.py
+++ controlled.py
@@ -43,7 +43,7 @@ def send_raw(data):
         return
     
     try:
-        connection.sendall(bytes("\n" + format(len(data) + 1, '04d')) + data)
+        connection.sendall(b"\n" + bytes(format(len(data)) + 1, '04d') + data)
     except Exception as e:
         print("Error sending text: {}".format(str(e)))
 
